edca79c544a2af706ff3b3f616fd105007d4bee7
carefully-do-homework <646989546@qq.com>
2022-12-18 17:17:38 +0800

    修改等出样式

--- yii2-ecommerce/frontend/views/layouts/main.php
+++ yii2-ecommerce/frontend/views/layouts/main.php
@@ -52,7 +52,7 @@ AppAsset::register($this);
         echo Html::beginForm(['/site/logout'], 'post', ['class' => 'd-flex'])
             . Html::submitButton(
                 'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout text-decoration-none']
+                ['class' => 'btn btn-link logout text-decoration-none', 'style' => 'color: var(--bs-nav-link-color)']
             )
             . Html::endForm();
     }
